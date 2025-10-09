ca093ed87568034daae0801b3c7e8e5e72428d84
SatyaJ <saphin.rao@gmail.com>
2025-10-09 15:38:56 -0700

    Added LaunchPage.java, LaunchPageStepDefinition

--- src/test/java/stepdefinition/LaunchPageStepDefinition.java
+++ src/test/java/stepdefinition/LaunchPageStepDefinition.java
@@ -1,71 +1,91 @@
 package stepdefinition;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
 
 import DriverManager.DriverFactory;
-import io.cucumber.java.en.*;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import pages.LaunchPage;
 
 public class LaunchPageStepDefinition {
-	
-	WebDriver driver;
-	
+
+	WebDriver driver = DriverFactory.getDriver();
+	LaunchPage launchPage = new LaunchPage(DriverFactory.getDriver());
+
 	@Given("the user has a browser open")
 	public void the_user_has_a_browser_open() {
-		
-		driver = DriverFactory.getDriver();
 
+		launchPage.verifyBrowserIsOpen();
 
 	}
 
 	@When("the user enters the correct DS Algo portal URL")
 	public void the_user_enters_the_correct_ds_algo_portal_url() {
 
+		launchPage.verifyLaunchUrl();
 
 	}
 
-	@Then("the user should be able to see the DS Algo portal with Preparing for the Interviews, Get Started button and Copyright info")
-	public void the_user_should_be_able_to_see_the_ds_algo_portal_with_preparing_for_the_interviews_get_started_button_and_copyright_info() {
+	@Then("the user should be able to see the DS Algo portal")
+	public void the_user_should_be_able_to_see_the_ds_algo_portal() {
 
+		Assert.assertTrue(launchPage.isPreparingTextVisible(), "Interview prep text not visible");
 
 	}
 
 	@Given("the user is on the DS Algo Portal page")
 	public void the_user_is_on_the_ds_algo_portal_page() {
 
+		launchPage.verifyLaunchUrl();
 
 	}
 
 	@When("the launch page loads")
 	public void the_launch_page_loads() {
-
+		Assert.assertTrue(launchPage.isPreparingTextVisible(), "Interview prep text not visible");
 
 	}
 
 	@Then("the user should be able to see {string}")
-	public void the_user_should_be_able_to_see(String string) {
+	public void the_user_should_be_able_to_see(String prepInterviews) {
 
+		Assert.assertTrue(launchPage.isPreparingTextVisible(), "Text '" + prepInterviews + "' not visible");
 
 	}
 
 	@Then("the user should be able to see {string} button")
-	public void the_user_should_be_able_to_see_button(String string) {
+	public void the_user_should_be_able_to_see_button(String getStartedText) {
 
+		Assert.assertTrue(launchPage.isGetStartedButtonVisible(getStartedText),
+				"Button '" + getStartedText + "' not visible");
+
+	}
+
+	@Then("the user should be able to see {string} copyrightInfo")
+	public void the_user_should_be_able_to_see_copyrightInfo(String copyrightInfo) {
+
+		WebElement copyright = driver.findElement(By.xpath("//*[contains(text(),'Copyright@NumpyNinja2021')]"));
+		Assert.assertTrue(copyright.isDisplayed(), "Text '" + copyrightInfo + " ' not visible");
 
 	}
 
 	@When("the user clikcs the {string} button")
-	public void the_user_clikcs_the_button(String string) {
+	public void the_user_clikcs_the_button(String GetStartedButtonText) {
 
-
-	}
-
-	@Then("the user shold be navigated to home page with NumpyNinja, Data Structures, Register and Sign in links")
-	public void the_user_shold_be_navigated_to_home_page_with_numpy_ninja_data_structures_register_and_sign_in_links() {
-
+		launchPage.clickGetStartedButton(GetStartedButtonText);
+		Assert.assertTrue(launchPage.isGetStartedButtonEnabled(GetStartedButtonText),
+				"Get Started button not clickable");
 
 	}
 
+	@Then("the user should be navigated to home page")
+	public void the_user_should_be_navigated_to_home_page() {
 
-
+		launchPage.getCurrentUrl();
+	}
 
 }
--- src/test/resources/features/LaunchPage.feature
+++ src/test/resources/features/LaunchPage.feature
@@ -3,7 +3,7 @@ Feature: DS Algo Portal functional testing
 Scenario: User opens the launch page
 Given the user has a browser open
 When the user enters the correct DS Algo portal URL
-Then the user should be able to see the DS Algo portal with Preparing for the Interviews, Get Started button and Copyright info
+Then the user should be able to see the DS Algo portal
        
 Scenario: Launch page shows "Preparing for the Interviews" 
 Given the user is on the DS Algo Portal page
@@ -23,17 +23,6 @@ Then the user should be able to see "Copyright info"
 Scenario: Navigation from Launch to home Page 
 Given the user is on the DS Algo Portal page
 When the user clikcs the "Get Started " button
-Then the user shold be navigated to home page 
+Then the user should be navigated to home page 
 
 
-Scenario Outline: Presence of "<links>" link in home page 
-Given the user is in launch page 
-When the  user clicks on "Get Started " button
-Then the user should see "<links>"
-
-Examples: 
- |links|
- |Numpy ninja|
- |Data Structures|
- |Register|
- |Sign in|
